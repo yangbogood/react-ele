7fa1e8ac21bcf72dda2aa86dbbfb86b30c7cbe7a
yangbo_vendo <yangbo_vendor@sensetime.com>
2019-12-06 11:14:47 +0800

    Initial the page of project

---
+++ precache-manifest.93ad0dcc39c5aaf82104b388ce7e6303.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498ca4a10e8ae438824d73f5f8627c12",
+    "url": "/react-ele/index.html"
+  },
+  {
+    "revision": "0adb32ed562f53b9c7e4",
+    "url": "/react-ele/static/css/2.4ce301a3.chunk.css"
+  },
+  {
+    "revision": "232292422f9554744694",
+    "url": "/react-ele/static/css/main.362299d2.chunk.css"
+  },
+  {
+    "revision": "0adb32ed562f53b9c7e4",
+    "url": "/react-ele/static/js/2.ec7ddf7c.chunk.js"
+  },
+  {
+    "revision": "232292422f9554744694",
+    "url": "/react-ele/static/js/main.079501bd.chunk.js"
+  },
+  {
+    "revision": "5b7400d7df72e38eb99c",
+    "url": "/react-ele/static/js/runtime~main.42edcc7d.js"
+  },
+  {
+    "revision": "328c50c945d4c1d5630c92a63587fcdd",
+    "url": "/react-ele/static/media/header-image.328c50c9.jpg"
+  },
+  {
+    "revision": "2686907c3fce6ec8702a468dbd49301e",
+    "url": "/react-ele/static/media/history.2686907c.png"
+  },
+  {
+    "revision": "25841959f589d746d60745bd52e7b237",
+    "url": "/react-ele/static/media/iconfont.25841959.eot"
+  },
+  {
+    "revision": "6f0032d0c052cdb68fb96c8a53ffa596",
+    "url": "/react-ele/static/media/iconfont.6f0032d0.ttf"
+  },
+  {
+    "revision": "c654f83bb94494dcc2057068de010e66",
+    "url": "/react-ele/static/media/iconfont.c654f83b.svg"
+  }
+]);
